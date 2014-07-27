9f6a231423bb0d7dbf903650b2806e3dafe51f2a
joe <jemoster@gmail.com>
2014-07-27 15:56:26 -0700

    Added test cases for executing basic .asm files

--- py/lambdaman_cpu_test.py
+++ py/lambdaman_cpu_test.py
@@ -5,7 +5,6 @@ from lambdaman_cpu import LambdaManCPU
 
 
 class TestLambdaManCPU_Interpretter(unittest.TestCase):
-
     def test_loading_program(self):
         cpu = LambdaManCPU()
         cpu.load('./test/test.asm')
@@ -18,18 +17,49 @@ class TestLambdaManCPU_Interpretter(unittest.TestCase):
     def test_executing_fib(self):
         cpu = LambdaManCPU()
         cpu.load('./test/fib.asm')
-        cpu.print_prog()
 
         limit = 100
-        limited = False
+        for _ in cpu:
+            if limit <= 0:
+                self.assertTrue(False, 'Too many steps execute.  Something is broken')
+                break
+            limit -= 1
 
-        #cpu.print_state()
-        for tick in cpu:
-            #cpu.print_state()
-            if limited:
-                if limit <= 0:
-                    break
-                limit -= 1
+    def test_executing_goto(self):
+        cpu = LambdaManCPU()
+        cpu.load('./test/goto.asm')
+
+        limit = 100
+        for _ in cpu:
+            if limit <= 0:
+                break
+            limit -= 1
+
+    def test_executing_tmp(self):
+        cpu = LambdaManCPU()
+        cpu.load('./test/tmp.asm')
+
+        limit = 100
+        for _ in cpu:
+            if limit <= 0:
+                self.assertTrue(False, 'Too many steps execute.  Something is broken')
+                break
+            limit -= 1
+
+    def test_executing_test(self):
+        cpu = LambdaManCPU()
+        cpu.load('./test/test.asm')
+
+        limit = 100
+        for _ in cpu:
+            if limit <= 0:
+                self.assertTrue(False, 'Too many steps execute.  Something is broken')
+                break
+            limit -= 1
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
