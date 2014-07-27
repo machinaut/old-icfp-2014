6b0612ac1cd748c35cb08cb1dd3f8cf54c230420
Alex Ray <a@machinaut.com>
2014-07-27 03:13:39 -0700

    add "dprint" debug-print-with-id to asm.s

--- gcc/samples/asm.s
+++ gcc/samples/asm.s
@@ -17,3 +17,13 @@ swap:
 ;  effectively pops the top of the stack off
 pop:
     RTN
+
+; dprint, call with two arguments: thing to print, and ID to print with
+;   returns the thing to print (so this evaluates as an expression)
+dprint:
+    LD 0 1  ; load ID
+    DBUG    ; print ID
+    LD 0 0  ; load thing to print
+    DBUG    ; print thing
+    LD 0 0  ; load thing again as our "return" value
+    RTN
